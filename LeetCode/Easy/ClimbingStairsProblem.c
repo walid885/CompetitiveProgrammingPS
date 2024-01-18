7c38a4490c35238183ed6c2f63c0ff823b35bd20
walid885 <bettaieb88walid@gmail.com>
2024-01-18 23:07:58 +0100

    problem solved , the idea was tricky

--- LeetCode/Easy/ClimbingStairsProblem.c
+++ LeetCode/Easy/ClimbingStairsProblem.c
@@ -11,28 +11,33 @@ an other even number
             2+2+1+1
             2+2+2
 so , basically we are trying to decompose that number into a sequance of one and twwos 
+
+
+
+here is the idea : found it in the solutions part in leetcode problemes : 
+Approach 1(Math)
+Initialization: Start with ways = 1, considering the default way of taking all steps at once.
+
+Iterative Approach:
+
+Iterate from 1 to n / 2 to explore different step sizes.
+For each step size, calculate a product using a nested loop, involving a combinatorial formula (C(n, k) = n! / (k! * (n - k)!)).
+Accumulation: Accumulate the product in the sum variable.
+
+Result: Add the sum to the ways variable for each step size.
+
+Return: The final value of ways represents the total number of distinct ways to climb the stairs
 */
 int climbStairs(int n) {
-    int res = 0 ; 
-    int temp = n ; 
-    if (n % 2 == 0 ){
-        while (temp != 0){
-            temp = temp /2 ; 
-            res += 1 ;
-            
-        }
-    }else {
-        res = 1 ; 
-         while (temp != 0){
-            printf(" tmp 0 : %d \n",temp);
-             temp = temp /2 ;
-             printf(" tmp 1 : %d\n ",temp);
-
-            res += 1 ;
-             
-        }
-    }
-    return res; 
+int res = 1 ; 
+  for (int i = 1 ; i <= n/2 ; i++ ){
+      double  sum = 1 ; 
+      for (int j = i ; j < 2*i ; j++ ){
+          sum *= (double)(n-j)/(j-i+1);
+      }
+      res += sum; 
+  }
+    return res ; 
 }
 void main (){
     climbStairs(1);
