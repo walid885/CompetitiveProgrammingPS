17883627a08ffd5be44eb9ce0008d63c85202924
walid885 <bettaieb88walid@gmail.com>
2024-01-18 22:09:25 +0100

    ideas about solving my first problem

---
+++ LeetCode/Easy/ClimbingStairsProblem.c
@@ -0,0 +1,14 @@
+/*
+the problem is about climbing the stairs, we have too options, we can go either one step or 2 steps at a time. 
+there are basically two cases, if the number is even or odd. 
+even : exemple 4 
+we can have 1 + 1 +1 +1 
+            2 +1 +1 
+            2 +2 
+an other even number 
+        6 : 1+1+ ... + 1 
+            2+1+1+1+1
+            2+2+1+1
+            2+2+2
+so , basically we are trying to decompose that number into a sequance of one and twwos 
+*/
