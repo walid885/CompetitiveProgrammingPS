41d981c0acd84119806a7fc9ef4f589d9da8be3e
walid885 <bettaieb88walid@gmail.com>
2024-01-22 18:30:31 +0100

    idea for solving the problem

---
+++ LeetCode/Easy/1672-RichestCustomerWealth.c
@@ -0,0 +1,7 @@
+/*
+this is my try in order to solve the problem Richest customer wealth. 
+leet code , following the Leetcode beginner Guide 
+the idea here is just simple , for each client ( each row in the matrix) we are 
+going to calculate his richness , and then , we are going to  store them in an array
+and after that , we are going to search the max of the array 
+*/
