0d47d8bdb73a57b12514ad68ce7c0fcb013a013e
walid885 <bettaieb88walid@gmail.com>
2024-01-23 20:40:27 +0100

    adding the comment section of this problem  and link for the further information about the snprintf function in C

--- LeetCode/  412_Fizz_Buzz.c
+++ LeetCode/  412_Fizz_Buzz.c
@@ -8,6 +8,7 @@
  * 
 */
 
+#include <cstddef>
 char** fizzBuzz(int n, int* returnSize) {
     char** res = (char**)malloc((n) * sizeof(char*));
     for (int i = 0; i < n; i++) {
@@ -32,3 +33,30 @@ char** fizzBuzz(int n, int* returnSize) {
     return res;
 }
 
+/**
+ *  What I have learnt through this problem 
+ * the usage of the dynamic memory for strings is basically , having the array of strings, and 
+ * in c we consider each string as an array 
+ * 
+ * there was a challenging problem in the buffer overflow, and hoenstly I don't know why it happend
+ * so It was a little bit tricky to iterate that number of times , but calculate upon the (i+1) , because i is 
+ * starting from 0 , and i DOn't know why it couldln't just iterate from 1 ----->n 
+ * 
+ * 
+ * it is the first time dealing and using the snprintf functionality 
+ * 
+ * here is some documnetation about it : "int snprintf ( char * s, size_t n, const char * format, ... );
+Write formatted output to sized buffer
+Composes a string with the same text that would be printed if format was used on printf, but instead of being printed, the content is stored as a C string in the buffer pointed by s (taking n as the maximum buffer capacity to fill).
+
+If the resulting string would be longer than n-1 characters, the remaining characters are discarded and not stored, but counted for the value returned by the function.
+
+A terminating null character is automatically appended after the content written.
+
+After the format parameter, the function expects at least as many additional arguments as needed for format.
+"
+
+and here is the link of it : " https://cplusplus.com/reference/cstdio/snprintf/"
+
+happy CODING, and consistancy is the key ! 
+*/
